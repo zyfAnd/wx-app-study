b81374fbdf764697c3e00e8e9eb829b47f565236
zyfand <1105564280@qq.com>
2017-11-30 13:59:03 +0800

    first commit

---
+++ liwushuo/pages/list/list.js
@@ -0,0 +1,26 @@
+Page({
+  data: {
+    list: {}
+  },
+  onReady: function () {
+    wx.setNavigationBarTitle({
+      title: this.title
+    })
+  },
+  onLoad: function (options) {
+    var that = this
+    this.title = options.title
+    wx.request({
+      url: 'https://www.daliandaxue.cn/wxapi/liwushuo_literature',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      success: function (res) {
+         that.setData({
+           list: res.data.stories
+         })
+         console.log(res.data)
+      }
+    })
+  }
+})
---
+++ liwushuo/utils/api.js
@@ -0,0 +1,2 @@
+var GIRLFRIEND_URL = 'https://www.daliandaxue.cn/wxapi/liwushuo_girlfriend';
+var LITERATURE_URL = 'https://www.daliandaxue.cn/wxapi/liwushuo_literature';
